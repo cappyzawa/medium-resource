7bd1a3cc8cc82d5cd5f008efd4e9eab70e267b0e
cappyzawa <cappyzawa@yahoo.ne.jp>
2019-02-23 02:36:27 +0900

    add medium inteface for operationg

--- medium.go
+++ medium.go
@@ -1,1 +1,11 @@
 package resource
+
+import "github.com/Medium/medium-sdk-go"
+
+// MediumClient can access to medium
+type MediumClient interface {
+	// https://godoc.org/github.com/Medium/medium-sdk-go#Medium.CreatePost
+	CreatePost(o medium.CreatePostOptions) (*medium.Post, error)
+	// https://godoc.org/github.com/Medium/medium-sdk-go#Medium.GetUser
+	GetUser(userID string) (*medium.User, error)
+}
