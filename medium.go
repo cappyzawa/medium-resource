383b4492707491187d8dc0832c346b4c232f8f31
cappyzawa <cappyzawa@yahoo.ne.jp>
2019-02-23 03:15:47 +0900

    add fake

--- medium.go
+++ medium.go
@@ -3,6 +3,7 @@ package resource
 import "github.com/Medium/medium-sdk-go"
 
 // MediumClient can access to medium
+//go:generate counterfeiter -o fakes/medium_client.go MediumClient
 type MediumClient interface {
 	// https://godoc.org/github.com/Medium/medium-sdk-go#Medium.CreatePost
 	CreatePost(o medium.CreatePostOptions) (*medium.Post, error)
