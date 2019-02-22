661bdfb13db888754dd49d8c2e1ea2261d3904db
cappyzawa <cappyzawa@yahoo.ne.jp>
2019-02-23 02:37:04 +0900

    add minimum out logic

--- out/command.go
+++ out/command.go
@@ -1,1 +1,41 @@
 package out
+
+import (
+	"github.com/Medium/medium-sdk-go"
+	"github.com/cappyzawa/medium-resource"
+)
+
+type Command struct {
+	MediumClient resource.MediumClient
+}
+
+// Run publish an article based on request.
+func (c *Command) Run(sourceDir string, request Request) (*Response, error) {
+	u, err := c.MediumClient.GetUser("")
+	if err != nil {
+		return nil, err
+	}
+	// https://godoc.org/github.com/Medium/medium-sdk-go#CreatePostOptions
+	o := medium.CreatePostOptions{
+		UserID:  u.ID,
+		Title:   "title",
+		Content: "#hoge",
+		// https://godoc.org/github.com/Medium/medium-sdk-go#ContentFormat
+		ContentFormat: medium.ContentFormat(request.Params.Format),
+		Tags:          request.Params.Tags,
+		CanonicalURL:  request.Params.CanonicalURL,
+		// https://godoc.org/github.com/Medium/medium-sdk-go#PublishStatus
+		PublishStatus: medium.PublishStatus(request.Params.Status),
+		// https://godoc.org/github.com/Medium/medium-sdk-go#License
+		License: medium.License(request.Params.License),
+	}
+	_, err = c.MediumClient.CreatePost(o)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Response{
+		resource.Version{},
+		[]resource.MetadataPair{},
+	}, nil
+}
--- out/models.go
+++ out/models.go
@@ -1,1 +1,22 @@
 package out
+
+import "github.com/cappyzawa/medium-resource"
+
+type Request struct {
+	Source resource.Source `json:"source"`
+	Params Params          `json:"params"`
+}
+
+type Params struct {
+	Format       string   `json:"format"`
+	File         string   `json:"file"`
+	Tags         []string `json:"tags"`
+	CanonicalURL string   `json:"canonical_url"`
+	Status       string   `json:"status"`
+	License      string   `json:"license"`
+}
+
+type Response struct {
+	Version  resource.Version
+	Metadata []resource.MetadataPair
+}
