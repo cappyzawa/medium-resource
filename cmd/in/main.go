ca22284af8cc4d133d28808dc25d6e944add5162
cappyzawa <cappyzawa@yahoo.ne.jp>
2019-02-24 01:12:56 +0900

    fix in

--- cmd/in/main.go
+++ cmd/in/main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
+
+	"github.com/cappyzawa/medium-resource/in"
 )
 
 func main() {
-	fmt.Fprintln(os.Stdout, "{\"version\":{\"id\": \"\"}}")
+	var req in.Request
+	if err := json.NewDecoder(os.Stdin).Decode(&req); err != nil {
+		fmt.Fprintf(os.Stderr, "invalid payload: %s\n", err)
+		os.Exit(1)
+	}
+	fmt.Fprintf(os.Stdout, "{\"version\":{\"id\": \"%s\"}}", req.Version.ID)
 }
